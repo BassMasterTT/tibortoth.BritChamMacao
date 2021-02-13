4ca01d52bf20e81ef8f402b5ef3a5dec494ccdb6
BassMasterTT <tothtibi22@gmail.com>
2021-02-13 14:34:06 +0800

    first commit

---
+++ tibortoth.BritChamMacao/frontity.settings.js
@@ -0,0 +1,44 @@
+const settings = {
+  name: "tibortoth",
+  state: {
+    frontity: {
+      url: "https://test.frontity.org",
+      title: "Test Frontity Blog",
+      description: "WordPress installation for Frontity development",
+    },
+  },
+  packages: [
+    {
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["About us", "/about-us/about-the-chamber/"],
+            ["Membership", "/membership/why-join/"],
+            ["Commitees", "/committes/the-britcham-committees/"],
+            ["CSR", "/corporate-social-responsibility-csr/"],
+            ["Join", "/membership/why-join/"],
+          ],
+
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
+    },
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://britchammacao.org/",
+        },
+      },
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+  ],
+};
+
+export default settings;
